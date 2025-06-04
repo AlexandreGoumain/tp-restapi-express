04f99e129d1f7f302639e4e193b521ebf5480ecc
Romain <romain.sales31@gmail.com>
2025-06-04 16:52:13 +0200

    feat(db):  add pictureUrl to get method

--- server/src/models/users.model.ts
+++ server/src/models/users.model.ts
@@ -29,6 +29,7 @@ export const userModel = {
                 columns: {
                     id: true,
                     username: true,
+                    pictureUrl: true,
                 },
                 with: {
                     spots: {
