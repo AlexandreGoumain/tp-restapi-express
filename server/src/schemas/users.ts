d626bd46cf63a8f8b895780cc68af93a7433f494
Romain <romain.sales31@gmail.com>
2025-06-05 18:52:27 +0200

    fix(register): fix unique password issue

--- server/src/schemas/users.ts
+++ server/src/schemas/users.ts
@@ -4,7 +4,7 @@ export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
     username: varchar('username', { length: 50 }).notNull().unique(),
     email: varchar('email', { length: 255 }).notNull().unique(),
-    password: varchar('password', { length: 255 }).notNull().unique(),
+    password: varchar('password', { length: 255 }).notNull(),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     modifiedAt: timestamp('modified_at').notNull().defaultNow(),
 });
