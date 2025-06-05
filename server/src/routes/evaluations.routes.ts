718126df6b071e16e62ce64c385b628f4299d649
Romain <romain.sales31@gmail.com>
2025-06-05 15:36:46 +0200

    feat(route): add middleware to check uuid url params

--- server/src/routes/evaluations.routes.ts
+++ server/src/routes/evaluations.routes.ts
@@ -1,26 +1,27 @@
 import { Router } from 'express';
 import evaluationsController from '../controllers/evaluations.controller';
 import { isAuthenticated } from '../middlewares';
+import { validateUUIDParam } from '../middlewares/validateUUIDParam';
 
 const router = Router();
 
 // GET http:///localhost:3000/evaluations/spots/{id_spot} -> récupérer toutes les evaluations d'un spot en fonction de l'id du spot
-router.get('/spots/:id_spot', evaluationsController.getAllBySpot);
+router.get('/spots/:id_spot', validateUUIDParam("id_spot"), evaluationsController.getAllBySpot);
 
 // GET http:///localhost:3000/evaluations/users/{id_user} -> récupérer toutes les evaluations d'un user en fonction de l'id du user
-router.get('/users/:id_user', evaluationsController.getAllByUser);
+router.get('/users/:id_user', validateUUIDParam("id_user"), evaluationsController.getAllByUser);
 
 // GET http:///localhost:3000/evaluations/25 -> récupérer un commentaire en fonction de son id
-router.get('/:id', evaluationsController.get);
+router.get('/:id', validateUUIDParam("id"), evaluationsController.get);
 
 // [POST] -     http://localhost:3000/evaluations -> créer un commentaire
 router.post('/', isAuthenticated, evaluationsController.create);
 
 // [PUT] -     http://localhost:3000/evaluations/25 -> éditer un commentaire
-router.put('/:id', isAuthenticated, evaluationsController.update);
+router.put('/:id', validateUUIDParam("id"), isAuthenticated, evaluationsController.update);
 
 // [DELETE] -     http://localhost:3000/evaluations/25 -> supprimer un commentaire
-router.delete('/:id', isAuthenticated, evaluationsController.delete);
+router.delete('/:id', validateUUIDParam("id"), isAuthenticated, evaluationsController.delete);
 
 export default router;
 
